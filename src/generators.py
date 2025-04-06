e718b751e56054ca277e7dfc6ddef26b8c451430
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-04-06 20:17:42 +0300

    update data, main.py

--- src/generators.py
+++ src/generators.py
@@ -1,53 +1,5 @@
+import json
 from typing import Any
-
-transaction_list = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
-
 
 def filter_by_currency(transaction_list: list, currency: str) -> Any:
     """Функция обрабатывает список транзакций и поочередно
@@ -72,9 +24,14 @@ def filter_by_currency(transaction_list: list, currency: str) -> Any:
         )
 
 
-result = filter_by_currency(transaction_list, currency="USD")
-for transaction in result:
-    print(transaction)
+if __name__ == '__main__':
+    with open("../data/data_for_example", "r", encoding="utf-8") as file:
+        input_information = file.readlines()
+        transaction_list = [json.loads(line) for line in input_information[22:26]]
+
+    result = filter_by_currency(transaction_list, currency="USD")
+    for transaction in result:
+        print(transaction)
 
 
 def transaction_descriptions(transaction_list: list) -> Any:
@@ -87,9 +44,10 @@ def transaction_descriptions(transaction_list: list) -> Any:
             yield transaction.get("description", "Ошибка! Отсутствует описание транзакции")
 
 
-item = transaction_descriptions(transaction_list)
-for transaction in item:
-    print(transaction)
+if __name__ == '__main__':
+    item = transaction_descriptions(transaction_list)
+    for transaction in item:
+        print(transaction)
 
 
 def card_number_generator(start: int, stop: int) -> Any:
@@ -112,13 +70,14 @@ def card_number_generator(start: int, stop: int) -> Any:
             current += 1
 
 
-try:
-    start = 1
-    stop = 6
+if __name__ == '__main__':
+    try:
+        start = 1
+        stop = 6
 
-    generator = card_number_generator(start, stop)
-    for card in generator:
-        print(card)
+        generator = card_number_generator(start, stop)
+        for card in generator:
+            print(card)
 
-except (TypeError, ValueError) as e:
-    print(e)
+    except (TypeError, ValueError) as e:
+        print(e)
