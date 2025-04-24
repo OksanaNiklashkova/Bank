d6166e7d5cf15e4bb57f421e05dd871b26f07cdb
Oksana Niklashkova <o.niklashkova@gmail.com>
2025-04-24 21:25:22 +0300

    add tests for reading-functions

--- src/tables_reading.py
+++ src/tables_reading.py
@@ -1,9 +1,12 @@
+from typing import Any
+
 import pandas as pd
 import os
 import csv
 
 
-def get_transactions_csv(file_path=None):
+def get_transactions_csv(file_path: str|None = None) -> Any:
+    """Функция считывает транзакции из файла .csv и возвращает список словарей"""
     if not file_path:
         file_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(file_dir, '..', 'data', 'transactions.csv')
@@ -14,12 +17,17 @@ def get_transactions_csv(file_path=None):
             reader = csv.DictReader(file_csv, delimiter=";")
             for row in reader:
                 data_csv.append(row)
-        return data_csv
+            if len(data_csv) != 0:
+                return data_csv
+            else:
+                print("Данные имеют неверный формат!")
+
     except FileNotFoundError:
-        print("Ошибка! Файл не найден!")
+        print('Ошибка! Файл не найден!')
 
 
-def get_transactions_xlsx(file_path=None):
+def get_transactions_xlsx(file_path: str|None = None) -> Any:
+    """Функция считывает транзакции из файла .xlsx и возвращает список словарей"""
     if not file_path:
         file_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(file_dir, '..', 'data', 'transactions_excel.xlsx')
@@ -30,6 +38,8 @@ def get_transactions_xlsx(file_path=None):
         return data_xlsx
     except FileNotFoundError:
         print("Ошибка! Файл не найден!")
+    except Exception:
+        print('Данные имеют неверный формат!')
 
 if __name__ == '__main__':
     x = get_transactions_csv()
